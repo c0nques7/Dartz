e47bdc72cbb9bdc8becadd33525d5d212f2f8b35
c0nques7 <c0nques7@gmail.com>
2021-09-22 19:09:14 -0400

    Start of test env
    began ragdoll script for test env

---
+++ Assets/Assets/Scripts/ragdollEnable.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ragdollEnable : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        int layerMask = 1 << 8;
+
+        layerMask = ~layerMask;
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Confirm, hit.");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Miss.");
+            //next figure out how to get the gun to shoot a ray at the target
+        }
+        }
+    }
+   
+}
